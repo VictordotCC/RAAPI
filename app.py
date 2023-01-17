f4e5b176f936e09ff53a0a89946dae81588ea353
VictordotCC <victor.celis.c88@gmail.com>
2023-01-16 22:52:20 -0300

    sql modificaciones

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from models import db
+from usermodels import db as userdb
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -11,4 +12,8 @@ app.config['MONGODB_SETTINGS'] = {
     'port': 27017
 }
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
+userdb.init_app(app)
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class Proyecto(db.Document):
     nombre = db.StringField(required=True, max_length=200)
     descripcion = db.StringField(required=True, max_length=200)
     fechaCreacion = db.StringField(required=True, max_length=200)
-    estado = db.StringField(required=True, max_length=200)
+    estado = db.BooleanField(required=True)
     Latitud = db.StringField(required=True, max_length=200)
     Longitud = db.StringField(required=True, max_length=200)
 
@@ -16,4 +16,39 @@ class Proyecto(db.Document):
                 "estado": self.estado,
                 "Latitud": self.Latitud,
                 "Longitud": self.Longitud}
+
+class FuentedeRuido(db.Document):
+    nombre = db.StringField(required=True, max_length=200)
+    descripcion = db.StringField(required=True, max_length=200)
+    fechaCreacion = db.StringField(required=True, max_length=200)
+    estado = db.BooleanField(required=True)
+    Latitud = db.StringField(required=True, max_length=200)
+    Longitud = db.StringField(required=True, max_length=200)
+    proyecto = db.ReferenceField(Proyecto, required=True, reverse_delete_rule=db.CASCADE)
+
+    def to_json(self):
+        return {"nombre": self.nombre,
+                "descripcion": self.descripcion,
+                "fechaCreacion": self.fechaCreacion,
+                "estado": self.estado,
+                "Latitud": self.Latitud,
+                "Longitud": self.Longitud,
+                "proyecto": self.proyecto.to_json()}
+
+class Medicion(db.Document):
+    velocidad_viento = db.FloatField(required=True)
+    direccion_viento = db.FloatField(required=True)
+    valor_ruido = db.FloatField(required=True)
+    fuente_ruido = db.ReferenceField(FuentedeRuido, required=True, reverse_delete_rule=db.CASCADE)
+
+    def to_json(self):
+        return {"velocidad_viento": self.velocidad_viento,
+                "direccion_viento": self.direccion_viento,
+                "valor_ruido": self.valor_ruido,
+                "fuente_ruido": self.fuente_ruido.to_json()}
+
+
+
+
+
     
