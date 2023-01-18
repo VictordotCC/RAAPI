40032d980964d180079287dabd4cf661fe31a445
VictordotCC <victor.celis.c88@gmail.com>
2023-01-17 23:22:24 -0300

    Modelos

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from models import db
-from usermodels import db as userdb
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -16,4 +15,3 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-userdb.init_app(app)
--- models.py
+++ models.py
@@ -3,11 +3,11 @@ db = MongoEngine()
 
 class Proyecto(db.Document):
     nombre = db.StringField(required=True, max_length=200)
-    descripcion = db.StringField(required=True, max_length=200)
+    descripcion = db.StringField(required=False, max_length=200)
     fechaCreacion = db.StringField(required=True, max_length=200)
     estado = db.BooleanField(required=True)
-    Latitud = db.StringField(required=True, max_length=200)
-    Longitud = db.StringField(required=True, max_length=200)
+    Latitud = db.FloatField(required=False)
+    Longitud = db.FloatField(required=False)
 
     def to_json(self):
         return {"nombre": self.nombre,
@@ -17,13 +17,19 @@ class Proyecto(db.Document):
                 "Latitud": self.Latitud,
                 "Longitud": self.Longitud}
 
-class FuentedeRuido(db.Document):
+class AeroGenerador(db.Document):
     nombre = db.StringField(required=True, max_length=200)
-    descripcion = db.StringField(required=True, max_length=200)
     fechaCreacion = db.StringField(required=True, max_length=200)
-    estado = db.BooleanField(required=True)
-    Latitud = db.StringField(required=True, max_length=200)
-    Longitud = db.StringField(required=True, max_length=200)
+    estado = db.BooleanField(required=True, default=True)
+    UtmEste = db.FloatField(required=True)
+    UtmNorte = db.FloatField(required=True)
+    LWS6 = db.FloatField(required=True)
+    LWS8 = db.FloatField(required=True)
+    LWS10 = db.FloatField(required=True)
+    Hb = db.FloatField(required=True)
+    ModoOperacion = db.StringField(required=True, max_length=200)
+    Marca = db.StringField(required=True, max_length=200)
+    Modelo = db.StringField(required=True, max_length=200)
     proyecto = db.ReferenceField(Proyecto, required=True, reverse_delete_rule=db.CASCADE)
 
     def to_json(self):
@@ -35,19 +41,50 @@ class FuentedeRuido(db.Document):
                 "Longitud": self.Longitud,
                 "proyecto": self.proyecto.to_json()}
 
-class Medicion(db.Document):
-    velocidad_viento = db.FloatField(required=True)
-    direccion_viento = db.FloatField(required=True)
-    valor_ruido = db.FloatField(required=True)
-    fuente_ruido = db.ReferenceField(FuentedeRuido, required=True, reverse_delete_rule=db.CASCADE)
+class Receptor(db.Document):
+    nombre = db.StringField(required=True, max_length=200)
+    fechaCreacion = db.StringField(required=True, max_length=200)
+    estado = db.BooleanField(required=True, default=True)
+    UtmEste = db.FloatField(required=True)
+    UtmNorte = db.FloatField(required=True)
+    Descripcion = db.StringField(required=True, max_length=200)
+    Tipo = db.StringField(required=True, max_length=200)
+    proyecto = db.ReferenceField(Proyecto, required=True, reverse_delete_rule=db.CASCADE)
 
     def to_json(self):
-        return {"velocidad_viento": self.velocidad_viento,
-                "direccion_viento": self.direccion_viento,
-                "valor_ruido": self.valor_ruido,
-                "fuente_ruido": self.fuente_ruido.to_json()}
+        return {"nombre": self.nombre,
+                "descripcion": self.descripcion,
+                "fechaCreacion": self.fechaCreacion,
+                "estado": self.estado,
+                "Latitud": self.Latitud,
+                "Longitud": self.Longitud,
+                "proyecto": self.proyecto.to_json()}
 
 
+
+
+
+#Modelos de Usuario
+
+class User(db.Document):
+    username = db.StringField(required=True, max_length=200)
+    password = db.StringField(required=True, max_length=200)
+    email = db.StringField(required=True, max_length=200)
+    rol = db.StringField(required=True, max_length=200)
+
+    def to_json(self):
+        return {"username": self.username,
+                "password": self.password,
+                "email": self.email,
+                "rol": self.rol}
+
+class User_Proyecto(db.Document):
+    user = db.ReferenceField(User, required=True, reverse_delete_rule=db.CASCADE)
+    proyecto = db.ReferenceField(Proyecto, required=True, reverse_delete_rule=db.CASCADE)
+
+    def to_json(self):
+        return {"user": self.user.to_json(),
+                "proyecto": self.proyecto.to_json()}
 
 
 
